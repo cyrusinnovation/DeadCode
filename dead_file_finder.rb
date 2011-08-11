1ca73b216d551ed3e4f772eb38f82ac016380e0e
Maddev <maddev@ubuntu.(none)>
2011-08-11 13:11:01 -0400

    David: test files are only found if specified

--- dead_file_finder.rb
+++ dead_file_finder.rb
@@ -1,31 +1,33 @@
 class DeadFileFinder
-  def self.find_class_files root_path
+  def self.find_class_files root_path, include_tests = false
+    find_files_by_regex root_path, /\.rb$/, include_tests
+  end
+
+  def self.find_usage_files root_path, include_tests = false
+    find_files_by_regex root_path, /\.(rb|rake|erb|yml|yaml)$/, include_tests
+  end
+
+  def self.find_files_by_regex root_path, regex, include_tests
     files = []
     Find.find(root_path) do |path|
-      files << path if path =~ /\.rb$/
+      if path =~ regex && (include_tests || !path.include?("test"))
+        files << path
+      end
     end
     files
   end
 
-  def self.find_usage_files root_path
-    files = []
-    Find.find(root_path) do |path|
-      files << path if path =~ /\.(rb|rake|erb|yml|yaml)$/
-    end
-    files
-  end
-  
-  def self.find_rails_class_files rails_root
-    files = find_class_files "#{rails_root}/app/"
-    files += find_class_files "#{rails_root}/lib/"
+  def self.find_rails_class_files rails_root, include_tests = false
+    files = find_class_files "#{rails_root}/app/", include_tests
+    files += find_class_files "#{rails_root}/lib/", include_tests
   end
 
-  def self.find_rails_usage_files rails_root
+  def self.find_rails_usage_files rails_root, include_tests = false
     files = []
     ["app", "lib", "config", "db", "script"].each do |folder|
-      files += find_usage_files "#{rails_root}/#{folder}/"
+      files += find_usage_files "#{rails_root}/#{folder}/", include_tests
     end
     files
   end
-  
+
 end
--- dead_file_finder_test.rb
+++ dead_file_finder_test.rb
@@ -4,27 +4,27 @@ require "dead_file_finder"
 class DeadFileFinderTest < Test::Unit::TestCase
 
   def test_rails_class_files_are_found
-    results = DeadFileFinder.find_rails_class_files "test_data"
+    results = DeadFileFinder.find_rails_class_files "test_data", true
     assert results.include?("test_data/app/models/classes.rb")
     assert results.include?("test_data/lib/classes.rb")
     assert !results.include?("test_data/config/usages.rb")
   end
 
   def test_rails_usage_files_are_found
-    results = DeadFileFinder.find_rails_usage_files "test_data"
+    results = DeadFileFinder.find_rails_usage_files "test_data", true
     assert results.include?("test_data/app/models/classes.rb")
     assert results.include?("test_data/app/views/some_controller/remember_me.html.erb")
     assert results.include?("test_data/config/usages.rb")
   end
 
   def test_correct_class_files_are_found
-    results = DeadFileFinder.find_class_files "test_data"
+    results = DeadFileFinder.find_class_files "test_data", true
     assert results.include?("test_data/classes.rb")
     assert !results.include?("test_data/ignore_me.js")
   end
 
   def test_correct_usage_files_are_found
-    results = DeadFileFinder.find_usage_files "test_data"
+    results = DeadFileFinder.find_usage_files "test_data", true
     assert results.include?("test_data/classes.rb")
     assert results.include?("test_data/remember_me.html.erb")
     assert !results.include?("test_data/ignore_me.js")
